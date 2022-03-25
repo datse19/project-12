681e2920adbf425cfaca0cb3fe7b2326480ab0aa
Dawa <datse162009@gmail.com>
2022-03-25 16:32:47 -0400

    final edit

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -10,20 +10,20 @@ exports.authenticateUser = async (req, res, next) => {
     const credentials = auth(req);
 
     if (credentials) {
-        const user = await User.findOne ({where: {emailAddress: credentials.name}});
+        const user = await User.findOne ({ where: {emailAddress: credentials.name}});
 
         if(user) {
             const authenticated = bcrypt
                 .compareSync(credentials.pass, user.password);
 
                 if(authenticated) {
-                    console.log(`Authentication successful for user: ${user.firstName} ${user.lastName}`);
+                    console.log(`Authentication successful for username: ${user.firstName} ${user.lastName}`);
                     req.currentUser = user;
                 } else {
-                    message = `Authentication failure for user: ${user.firstName} ${user.lastName}`;   
+                    message = `Authentication failure for username: ${user.firstName} ${user.lastName}`;   
                 }
         } else {
-            message = `User not found for user: ${credentials.name}`;
+            message = `User not found for username: ${credentials.name}`;
         }
     } else {
         message = `Auth header not found`;
